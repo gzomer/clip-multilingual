5fa90d3d7bb71f2684a10ad14cfbbf3035697b6e
Gustavo Zomer <gustavozomer@gmail.com>
2022-02-22 12:31:53 +0100

    Improve search

--- search.py
+++ search.py
@@ -34,8 +34,11 @@ class MultiLingualSearch:
         logits_images, logits_text = self.compare_embeddings(logit_scale, images_tensors.to('cpu'), txt_embs.to('cpu'))
         return logits_images.softmax(dim=0).cpu().detach().numpy()
 
-    def search(self, text, amount=10):
-        probs = self.compare_text_images(self.model, text, self.images_embeddings)
-        images_probs = list(zip(self.images_data, [item[0] for item in probs.tolist()]))
+    def search_images(self, text, images_embeddings, images_data, amount=10):
+        probs = self.compare_text_images(self.model, text, images_embeddings)
+        images_probs = list(zip(images_data, [item[0] for item in probs.tolist()]))
         sorted_images = sorted(images_probs, key=lambda x:x[1], reverse=True)
         return [{'image': item[0], 'prob': item[1]} for item in sorted_images[:amount]]
+
+    def search(self, text, amount=10):
+        return self.search_images(text, self.images_embeddings, self.images_data, amount)
