2e2e46db8a524d687636a3533e83413a2d31f3da
Gustavo Zomer <gustavozomer@gmail.com>
2022-02-22 12:43:42 +0100

    Install CLIP

--- setup.py
+++ setup.py
@@ -8,12 +8,16 @@ setup(
     package_dir={'clip_multilingual': ''},
     packages=['clip_multilingual'],
     version='1.0.1',
-    description='MultiLingual CLIP',
+    description='MultiLingual CLIP - Semantic Image Search in 100 Languages',
     author='Gustavo Zomer',
+    nclude_package_data=True,
     install_requires=[
         str(r)
         for r in pkg_resources.parse_requirements(
             open(os.path.join(os.path.dirname(__file__), 'requirements.txt'))
         )
+    ],
+    dependency_links = [
+        'git+https://github.com/openai/CLIP.git#egg=clip',
     ],
 )
